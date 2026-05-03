939403393d80ef05ba41ff9fa9179038d92947ce
knowave <kwaktaemin98@gmail.com>
2026-05-03 21:47:50 +0900

    UPDATE: CORS .env로 수정

--- src/main/kotlin/com/knowave/cashboard/common/config/WebConfig.kt
+++ src/main/kotlin/com/knowave/cashboard/common/config/WebConfig.kt
@@ -1,14 +1,18 @@
 package com.knowave.cashboard.common.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+	@Value("\${cors.allowed-origin}")
+	private val allowedOrigin: String,
+) : WebMvcConfigurer {
 	override fun addCorsMappings(registry: CorsRegistry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:5173")
+			.allowedOrigins(allowedOrigin)
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true)
